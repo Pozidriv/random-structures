1ee16bd0391e87c81c89f40ba33ea1c2a8875502
Pozidriv <youri.tam@gmail.com>
2016-03-09 20:35:17 -0500

    Added files via upload

--- RBTree.java
+++ RBTree.java
@@ -11,8 +11,9 @@
 public class RBTree<K> {
 	int key;
 	K value;
-	BinaryTree<K> left;
-	BinaryTree<K> right;
+	RBTree<K> left;
+	RBTree<K> right;
+	RBTree<K> parent;
 	int size;
 	int rank;
 	boolean color;
@@ -21,21 +22,80 @@ public class RBTree<K> {
 		
 	}
 	
-	public RBTree(int k, K v,	BinaryTree<K> l, BinaryTree<K> r, int s, int rk, boolean c) {
+	public RBTree(int k, K v,	RBTree<K> l, RBTree<K> r, RBTree<K> p, int s, int rk, boolean c) {
 		key = k;
 		value = v;
 		left = l;
 		right = r;
+		parent = p;
 		size = s;
 		rank = rk;
 		color = c;
 	}
 	
-	public void insert(BinaryTree<K> x) {
+	public void insert(RBTree<K> x) {
+		if (x.key < this.key && this.left == null) {
+			this.left = x;
+			x.parent = this;
+			x.color = true;
+			x.rank = 1;
+			fix(x);
+		}
+		else if (x.key > this.key && this.right == null) {
+			this.right = x;
+			x.parent = this;
+			x.color = true;
+			x.rank = 1;
+			fix(x);
+		}
+		else if (x.key < this.key) {
+			this.left.insert(x);
+		}
+		else if (x.key > this.key) {
+			this.right.insert(x);
+		}
+		else {
+			/* Need to throw Exception in this case (x.key = this.key) */
+		}
+	}
+	
+	public void delete(RBTree<K> x) {
 		
 	}
 	
-	public void delete(BinaryTree<K> x) {
+	public static void fix(RBTree x) {
+		RBTree uncle;
+		
+		if (x.parent == null || x.parent.parent == null) return;
+		//Note that a RB tree is supposed to have a black root.
+		
+		uncle = x.parent.parent.otherChild(x.parent);
+		
+		if (!uncle.color) {
+			rotate(x, uncle);
+			return;
+		}
+		else {
+			x.parent.color = false;
+			uncle.color = false;
+			uncle.parent.color = true;
+			uncle.parent.rank = uncle.rank + 1;
+			fix(uncle.parent);
+		}
 		
 	}
+	
+	public static void rotate(RBTree x, RBTree u) {
+		x.parent.parent = u.parent.parent;
+		
+		u.parent.parent = x.parent;
+		
+		
+	}
+	
+	private RBTree<K> otherChild(RBTree<K> x) {
+		if (this.left == x) return this.right;
+		else return this.left;
+	}
+	
 }
